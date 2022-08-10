378f40228d98790ea3a5e871eaa09097d505ad93
Avinash <tavinash391gmail.com>
2022-08-10 17:17:53 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import  "./navBar.css"
+const NavBar = () => (
+    <header className='navbar'>
+        <div className='navbar__title navbar__item'>First Day</div>
+        <div className='navbar__item'>About Us</div>
+        <div className='navbar__item'>Contact</div>
+        <div className='navbar__item'>Help</div>        
+    </header>
+);
+
+export default NavBar;
